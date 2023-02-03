d457fe2cfbde9b48bb3d3b9d52f0af7cf532698b
Ting Hua Lin <coolting.lin3@gmail.com>
2023-02-03 22:21:58 +0100

    add rounding of numbers depend on number of decimals

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 let onenumber = document.querySelector("#firstnumber");
 let twonumber = document.querySelector("#secondnumber");
 let arithOperator = document.querySelector("#operator");
+let roundingCheckBox = document.querySelector("#doround"); //#doround is the id for input type="checkbox"
 let subOneNumber; // the new firstnumber after a calculation
-let dropdownOperator = "undefined"; //dropdownOperator var storing arithOperators indicies.
+let dropdownOperator = "undefined"; //dropdownOperator variable storing arithOperators indicies.
 //dropdown menu value set to undefined, if no input on dropdown has been detected, shows + by default, but no 'add' operation is occuring unless input changed
+
+let roundingNumber = document.querySelector("#decimals"); //a variable storing which decimal number the program should round
+let roundTo = "undefined";
 let listofCalcContainer = document.querySelector("#results"); //list of finished calcs
 
 window.addEventListener("DOMContentLoaded", setup);
@@ -20,18 +24,35 @@ function setup() {
     twonumber.value;
     console.log(twonumber.value); //how to get the value from input field and write to input field
   });
+  //eventListener on dropdown menu for operators
   arithOperator.addEventListener("input", () => {
     //read an dropdown value
     dropdownOperator = arithOperator.value;
     console.log(arithOperator.value);
   });
+
+  //eventListener on dropdown menu for rounding numbers
+  roundingCheckBox.addEventListener("change", () => {
+    //.checked built.in () to see if checkbox is bool true
+    if (roundingCheckBox.checked) {
+      console.log("roundingNumber.value");
+      //get the number rounding value
+      roundingNumber.addEventListener("input", () => {
+        roundTo = roundingNumber.value;
+        console.log(roundTo);
+      });
+    } else {
+      console.log("no rounding");
+    }
+  });
+
   document
     .querySelector("#calculate")
-    .addEventListener("click", () => calculate(dropdownOperator)); //parse the operator over to calc() in order to compare
+    .addEventListener("click", () => calculate(dropdownOperator, roundTo)); //parse the operator over to calc() in order to compare
 }
 
-function calculate(dropdownOperator) {
-  //if-statements that parses input-selected operator value and compares with the dropdown menues values.
+function calculate(dropdownOperator, roundTo) {
+  console.log("This is the rounding number");
   if (dropdownOperator === "add" || dropdownOperator === "undefined") {
     subOneNumber =
       // read the numbers from input field and calc_add them together into a new var
@@ -58,13 +79,20 @@ function calculate(dropdownOperator) {
       Number.parseInt(twonumber.value, 10);
   }
 
+  // check if result should be rounded, round it and then add corresponding result to list
+  if (roundingCheckBox.checked) {
+    listofCalcContainer.innerHTML +=
+      "<li>" + subOneNumber.toFixed(roundTo) + "</li>";
+    console.log("decimal placed");
+  } else {
+    listofCalcContainer.innerHTML += "<li>" + subOneNumber + "</li>";
+  }
   //after calculation, set #firstnumber input value to be the result of prev calculation,
   //clear inputfield for #secondnumber
   onenumber.value = subOneNumber;
   twonumber.value = "";
 
   //append result of prev calc to list#results
-  listofCalcContainer.innerHTML += "<li>" + subOneNumber + "</li>";
 
   //make list of results scrollable
   //get content of 'results' list in order to set scroll to the last added li item
@@ -72,5 +100,4 @@ function calculate(dropdownOperator) {
   let lastAddednewNumber = listofCalc[listofCalc.length - 1];
   console.log(lastAddednewNumber);
   lastAddednewNumber.scrollIntoView(false, { behaviour: "smooth" });
-  //listofCalc.querySelectorAll("li").scrollIntoView();
 }
