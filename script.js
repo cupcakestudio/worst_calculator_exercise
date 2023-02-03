239f75707d4e81556e4c1ad940869c0b9f370ce7
Ting Hua Lin <coolting.lin3@gmail.com>
2023-02-03 17:24:47 +0100

    add different math operations functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let onenumber = document.querySelector("#firstnumber");
 let twonumber = document.querySelector("#secondnumber");
 let arithOperator = document.querySelector("#operator");
 let subOneNumber; // the new firstnumber after a calculation
+let dropdownOperator = "undefined"; //dropdownOperator var storing arithOperators indicies.
+//dropdown menu value set to undefined, if no input on dropdown has been detected, shows + by default, but no 'add' operation is occuring unless input changed
 window.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
@@ -15,18 +17,45 @@ function setup() {
     twonumber.value;
     console.log(twonumber.value); //how to get the value from input field and write to input field
   });
+  arithOperator.addEventListener("input", () => {
+    //read an dropdown value
+    dropdownOperator = arithOperator.value;
+    console.log(arithOperator.value);
+  });
   document
     .querySelector("#calculate")
-    .addEventListener("click", () => calculate());
-  // arithOperator.addEventListener("input", () => console.log("plus"));
+    .addEventListener("click", () => calculate(dropdownOperator)); //parse the operator over to calc() in order to compare
 }
 
-function calculate() {
-  subOneNumber =
-    // read the numbers from input field and calc_add them together into a new var
-    Number.parseInt(onenumber.value, 10) +
-    // String(arithOperator.value) + // add operator here fron string conversion to arith Operators
-    Number.parseInt(twonumber.value, 10);
+function calculate(dropdownOperator) {
+  //if-statements that parses input-selected operator value and compares with the dropdown menues values.
+
+  if (dropdownOperator === "add" || dropdownOperator === "undefined") {
+    subOneNumber =
+      // read the numbers from input field and calc_add them together into a new var
+      Number.parseInt(onenumber.value, 10) +
+      // add operator here fron string conversion to arith Operators
+      Number.parseInt(twonumber.value, 10);
+  } else if (dropdownOperator === "sub") {
+    subOneNumber =
+      // read the numbers from input field and subtract them together into a new var
+      Number.parseInt(onenumber.value, 10) -
+      // add operator here fron string conversion to arith Operators
+      Number.parseInt(twonumber.value, 10);
+  } else if (dropdownOperator === "mul") {
+    subOneNumber =
+      // read the numbers from input field and multiply them together into a new var
+      Number.parseInt(onenumber.value, 10) *
+      // add operator here fron string conversion to arith Operators
+      Number.parseInt(twonumber.value, 10);
+  } else {
+    // read the numbers from input field and divide them together into a new var
+    subOneNumber =
+      Number.parseInt(onenumber.value, 10) /
+      // add operator here fron string conversion to arith Operators
+      Number.parseInt(twonumber.value, 10);
+  }
+
   //after calculation, set #firstnumber input value to be the result of prev calculation,
   //clear inputfield for #secondnumber
   onenumber.value = subOneNumber;
